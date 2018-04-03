a309fdc3c633720a43cbb569930e54cd879145a8
Gihun Ham <progh2@gmail.com>
2018-04-03 13:04:01 +0900

    #2 XML 의존 주입

--- src/main/java/kr/hs/emirim/progh2/greeter/main/Main.java
+++ src/main/java/kr/hs/emirim/progh2/greeter/main/Main.java
@@ -1,14 +1,22 @@
 package kr.hs.emirim.progh2.greeter.main;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import kr.hs.emirim.progh2.greeter.greeter.Greeter;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Greeter greeter = new Greeter();
-		greeter.setFormat("%s, 안녕하세요!");
-		String msg = greeter.greet("스프링"); //msg는 "스프링, 안녕하세요!"가 된다.
+//		Greeter greeter = new Greeter();
+//		greeter.setFormat("%s, 안녕하세요!");
+//		String msg = greeter.greet("스프링"); //msg는 "스프링, 안녕하세요!"가 된다.
+//		System.out.println(msg);
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("스프링");
 		System.out.println(msg);
+		ctx.close();
 	}
 
 }
